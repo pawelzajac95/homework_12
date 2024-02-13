605780a2691cf74039dd26df69e4a80de52cd09d
pawelzajac95 <pawelzajac34@wp.pl>
2024-02-13 19:40:07 +0100

    Update new_homework_12.py

--- new_homework_12.py
+++ new_homework_12.py
@@ -120,135 +120,137 @@ class AddressBook(UserDict):
     def __iter__(self):
         self.records = list(self.data.values())
         self.current_index = 0
+        self.page_size = 100
         return self
 
     def __next__(self):
-        if self.current_index >= len(self.records):
+        keys = list(self.data.keys())
+        if self.current_index >= len(keys):
             raise StopIteration
-        record = self.records[self.current_index]
-        self.current_index += 1
-        return record
+        keys_slice = keys[self.current_index:self.current_index + self.page_size]
+        self.current_index += self.page_size
+        return [self.data[key] for key in keys_slice]
 
 
 def main():
     address_book = AddressBook()
 
     while True:
-        # try:
-        command = input("Enter command: ")
-
-        if command == "hello":
-            print("How can I help you?")
-
-        elif command.startswith("add"):
-            _, first_name, last_name, phone_number, * \
-                birthday_input = command.split(" ", 4)
-            name = f'{first_name} {last_name}'
-            record = Record(name)
-            record.add_phone(phone_number)
-            if birthday_input:
-                try:
-                    birthday_date = datetime.strptime(
-                        birthday_input[0], "%Y-%m-%d")
-                    record.birthday = Birthday(birthday_date)
-                except ValueError:
-                    print(
-                        "Invalid birthday date format. Please enter date in YYYY-MM-DD format.")
-            address_book.add_record(record)
-            print("Contact added successfully.")
-
-        elif command.startswith("change"):
-            _, first_name, last_name, new_phone_number = command.split(
-                " ", 3)
-            name = f'{first_name} {last_name}'
-            record = address_book.find_record(name)
-            if record:
-                record.edit_phone(record.phones[0].value, new_phone_number)
-                print("Phone number updated successfully.")
-            else:
-                print("Contact not found.")
-
-        elif command.startswith("find"):
-            _, first_name, last_name = command.split(" ", 2)
-            name = f'{first_name} {last_name}'
-            found_records = address_book.find_record(name)
-            if found_records:
-                for phone in found_records.phones:
-                    print(f'{found_records.name.value} Phone: {
-                        phone.value}')
-                if found_records.birthday.value:
-                    print(f'{found_records.name.value} Birthday: {
-                        found_records.birthday.value.date()}')
-                    days_until_birthday = found_records.days_to_birthday()
-                    print(f"Days until next birthday: {
-                        days_until_birthday}")
-            else:
-                raise ValueError('Contact not found')
-
-        elif command.startswith("delete"):
-            _, first_name, last_name = command.split(" ", 2)
-            name = f'{first_name} {last_name}'
-            record = address_book.find_record(name)
-            if record:
-                record.remove_phone(address_book)
-                print('Contact deleted successfully')
-            else:
-                print("Contact not found.")
-
-        elif command.startswith("list"):
-            for page in (address_book):
-
-                for record in page:
-                    print(f"Name: {record.name.value}")
-                    for phone in record.phones:
-                        print(f"Phone: {phone.value}")
-                    if record.birthday.value:
-                        print(f"Birthday: {record.birthday.value.date()}")
-                        days_until_birthday = record.days_to_birthday()
+        try:
+            command = input("Enter command: ")
+    
+            if command == "hello":
+                print("How can I help you?")
+    
+            elif command.startswith("add"):
+                _, first_name, last_name, phone_number, * \
+                    birthday_input = command.split(" ", 4)
+                name = f'{first_name} {last_name}'
+                record = Record(name)
+                record.add_phone(phone_number)
+                if birthday_input:
+                    try:
+                        birthday_date = datetime.strptime(
+                            birthday_input[0], "%Y-%m-%d")
+                        record.birthday = Birthday(birthday_date)
+                    except ValueError:
+                        print(
+                            "Invalid birthday date format. Please enter date in YYYY-MM-DD format.")
+                address_book.add_record(record)
+                print("Contact added successfully.")
+    
+            elif command.startswith("change"):
+                _, first_name, last_name, new_phone_number = command.split(
+                    " ", 3)
+                name = f'{first_name} {last_name}'
+                record = address_book.find_record(name)
+                if record:
+                    record.edit_phone(record.phones[0].value, new_phone_number)
+                    print("Phone number updated successfully.")
+                else:
+                    print("Contact not found.")
+    
+            elif command.startswith("find"):
+                _, first_name, last_name = command.split(" ", 2)
+                name = f'{first_name} {last_name}'
+                found_records = address_book.find_record(name)
+                if found_records:
+                    for phone in found_records.phones:
+                        print(f'{found_records.name.value} Phone: {
+                            phone.value}')
+                    if found_records.birthday.value:
+                        print(f'{found_records.name.value} Birthday: {
+                            found_records.birthday.value.date()}')
+                        days_until_birthday = found_records.days_to_birthday()
                         print(f"Days until next birthday: {
                             days_until_birthday}")
-                print()
-
-        elif command.startswith("search"):
-            _, query = command.split(" ", 1)
-
-            found_contacts = record.search_contacts(
-                address_book, query)
-            if found_contacts:
-                print("Found contacts:")
-                for contact in found_contacts:
-                    print(f"Name: {contact.name.value}")
-                    for phone in contact.phones:
-                        print(f"Phone: {phone.value}")
-                    if contact.birthday.value:
-                        print(f"Birthday: {contact.birthday.value.date()}")
-                        days_until_birthday = contact.days_to_birthday()
-                        print(f"Days until next birthday: {
-                              days_until_birthday}")
-                    print()
+                else:
+                    raise ValueError('Contact not found')
+    
+            elif command.startswith("delete"):
+                _, first_name, last_name = command.split(" ", 2)
+                name = f'{first_name} {last_name}'
+                record = address_book.find_record(name)
+                if record:
+                    record.remove_phone(address_book)
+                    print('Contact deleted successfully')
+                else:
+                    print("Contact not found.")
+    
+            elif command.startswith("list"):
+                for page in (address_book):
+    
+                    for record in page:
+                        print(f"Name: {record.name.value}")
+                        for phone in record.phones:
+                            print(f"Phone: {phone.value}")
+                        if record.birthday.value:
+                            print(f"Birthday: {record.birthday.value.date()}")
+                            days_until_birthday = record.days_to_birthday()
+                            print(f"Days until next birthday: {
+                                days_until_birthday}")
+                        print()
+    
+            elif command.startswith("search"):
+                _, query = command.split(" ", 1)
+    
+                found_contacts = record.search_contacts(
+                    address_book, query)
+                if found_contacts:
+                    print("Found contacts:")
+                    for contact in found_contacts:
+                        print(f"Name: {contact.name.value}")
+                        for phone in contact.phones:
+                            print(f"Phone: {phone.value}")
+                        if contact.birthday.value:
+                            print(f"Birthday: {contact.birthday.value.date()}")
+                            days_until_birthday = contact.days_to_birthday()
+                            print(f"Days until next birthday: {
+                                  days_until_birthday}")
+                        print()
+                else:
+                    print("No contacts found.")
+    
+            elif command == 'save':
+                save_address_book(address_book, "address_book.txt")
+                print('Address book saved successfully.')
+    
+            elif command == 'load':
+                try:
+                    address_book = load_address_book('address_book.txt')
+                    print('Address book loaded successfully.')
+                except FileNotFoundError:
+                    print('No existing address book found')
+    
+            elif command in ["goodbye", "close", "exit", "."]:
+                print("Goodbye!")
+                break
+    
             else:
-                print("No contacts found.")
+                print("Invalid command. Please try again.")
 
-        elif command == 'save':
-            save_address_book(address_book, "address_book.txt")
-            print('Address book saved successfully.')
-
-        elif command == 'load':
-            try:
-                address_book = load_address_book('address_book.txt')
-                print('Address book loaded successfully.')
-            except FileNotFoundError:
-                print('No existing address book found')
-
-        elif command in ["goodbye", "close", "exit", "."]:
-            print("Goodbye!")
-            break
-
-        else:
-            print("Invalid command. Please try again.")
-
-        # except Exception as e:
-        # print(f'Error: {e}')
+        except Exception as e:
+            print(f'Error: {e}')
 
 
 if __name__ == "__main__":
